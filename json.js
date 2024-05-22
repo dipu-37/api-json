7ae74aeaa9a3f22bef6c33adc6093af3b6348fdc
dipu-37 <razibul1105nstu@gmail.com>
2024-05-22 11:43:16 +0600

    json parse stringify

---
+++ json.js
@@ -0,0 +1,8 @@
+// const txt = '{"name":"John", "age":30, "city":"New York"}'
+// const obj = JSON.parse(txt);
+// console.log(obj)
+
+
+const obj = {name: "John", age: 30, city: "New York"};
+const myJSON = JSON.stringify(obj);
+console.log(myJSON)
